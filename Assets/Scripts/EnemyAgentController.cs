5dc431b60d7d5b82830149a8d9c496bcaee8a88b
jafcor12 <74846120+jafcor12@users.noreply.github.com>
2023-12-11 00:25:52 -0600

    Testing

--- Assets/Scripts/EnemyAgentController.cs
+++ Assets/Scripts/EnemyAgentController.cs
@@ -15,10 +15,13 @@ public class EnemyAgentController : MonoBehaviour
 
     bool _hitState;
 
+    Collider colliderComponent;
+
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _animator = GetComponent<Animator>();
+        colliderComponent = GetComponent<Collider>();
     }
 
     // Update is called once per frame
@@ -36,6 +39,7 @@ public class EnemyAgentController : MonoBehaviour
         // Perform death animations, play sound, or destroy the enemy GameObject
         _hitState = true;
         _animator.SetBool("EnemyHit", true);
+        colliderComponent.enabled = false;
 
         StartCoroutine(WaitDelete());
     }
